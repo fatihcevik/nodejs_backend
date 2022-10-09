88eb901950c73b280f6e74afc9322ae54a646a05
fatihcevik <fatihcewik@gmail.com>
2022-10-09 13:03:36 +0300

    Auth Roles feature added

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -1,28 +1,50 @@
-module.exports = app => {
-    const tutorials = require("../controllers/tutorial.controller.js");
+const { authJwt } = require("../middleware");
+const tutorials = require("../controllers/tutorial.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post("/api/tutorials/", [authJwt.verifyToken], tutorials.create);
+  app.get("/api/tutorials/", [authJwt.verifyToken], tutorials.findAll);
+  app.get("/api/tutorials/published", [authJwt.verifyToken], tutorials.findAllPublished);
+  app.get("/api/tutorials/:id", [authJwt.verifyToken], tutorials.findOne);
+  app.put("/api/tutorials/:id", [authJwt.verifyToken], tutorials.update);
+  app.delete("/api/tutorials/:id", [authJwt.verifyToken], tutorials.delete);
+  app.delete("/api/tutorials/", [authJwt.verifyToken], tutorials.deleteAll);
+
+};
+
+// module.exports = app => {
+//     const tutorials = require("../controllers/tutorial.controller.js");
   
-    var router = require("express").Router();
+//     var router = require("express").Router();
   
-    // Create a new Tutorial
-    router.post("/", tutorials.create);
+//     // Create a new Tutorial
+//     router.post("/", tutorials.create);
   
-    // Retrieve all Tutorials
-    router.get("/", tutorials.findAll);
+//     // Retrieve all Tutorials
+//     router.get("/", tutorials.findAll);
   
-    // Retrieve all published Tutorials
-    router.get("/published", tutorials.findAllPublished);
+//     // Retrieve all published Tutorials
+//     router.get("/published", tutorials.findAllPublished);
   
-    // Retrieve a single Tutorial with id
-    router.get("/:id", tutorials.findOne);
+//     // Retrieve a single Tutorial with id
+//     router.get("/:id", tutorials.findOne);
   
-    // Update a Tutorial with id
-    router.put("/:id", tutorials.update);
+//     // Update a Tutorial with id
+//     router.put("/:id", tutorials.update);
   
-    // Delete a Tutorial with id
-    router.delete("/:id", tutorials.delete);
+//     // Delete a Tutorial with id
+//     router.delete("/:id", tutorials.delete);
   
-    // Create a new Tutorial
-    router.delete("/", tutorials.deleteAll);
+//     // Create a new Tutorial
+//     router.delete("/", tutorials.deleteAll);
   
-    app.use('/api/tutorials', router);
-  };
+//     app.use('/api/tutorials', router);
+//   };
--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 const db = require("./app/models");
+const Role = db.role;
+
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -18,13 +20,12 @@ db.mongoose
   })
   .then(() => {
     console.log("Connected to the database!");
+    initial();
   })
   .catch(err => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
-
-  console.log("we are continue");
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -38,9 +39,47 @@ app.get("/", (req, res) => {
 });
 
 require("./app/routes/tutorial.routes")(app);
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+  Role.collection.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
